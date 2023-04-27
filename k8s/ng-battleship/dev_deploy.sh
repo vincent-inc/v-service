035995a5daa0dfc74399e726610778c03477a063
VincentB00 <vincentbui1999@gmail.com>
2023-04-27 14:32:25 -0400

    fix k8s deployment

--- k8s/ng-battleship/dev_deploy.sh
+++ k8s/ng-battleship/dev_deploy.sh
@@ -1,4 +1,4 @@
-namespace=v-console-dev
+namespace=ng-battleship-dev
 microk8s kubectl delete namespace ${namespace}
 microk8s kubectl create namespace ${namespace}
 microk8s kubectl -n ${namespace} apply -f dev_deployment.yaml
--- k8s/ng-battleship/prod_deploy.sh
+++ k8s/ng-battleship/prod_deploy.sh
@@ -1,4 +1,4 @@
-namespace=v-console-prod
+namespace=ng-battleship-prod
 microk8s kubectl delete namespace ${namespace}
 microk8s kubectl create namespace ${namespace}
 microk8s kubectl -n ${namespace} apply -f prod_deployment.yaml
