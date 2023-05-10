48b16fe8c3074ab83e51d634267c402bf6522570
VincentB00 <vincentbui1999@gmail.com>
2023-05-10 12:41:10 -0400

    add lobbies fetch

--- src/app/game/lobby/lobby.component.ts
+++ src/app/game/lobby/lobby.component.ts
@@ -1,15 +1,54 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { first } from 'rxjs';
+import { Lobby, LobbyRow } from 'src/app/shared/model/VGame.model';
+import { VGameService } from 'src/app/shared/service/VGame.service';
 
 @Component({
   selector: 'app-lobby',
   templateUrl: './lobby.component.html',
   styleUrls: ['./lobby.component.scss']
 })
-export class LobbyComponent implements OnInit {
+export class LobbyComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  LobbyRows: LobbyRow[] = [];
+  private lobbyFetch?: any;
+
+  constructor(
+    private vgameService: VGameService
+  ) { }
+
+  ngOnDestroy(): void {
+    clearInterval(this.lobbyFetch);
+  }
 
   ngOnInit() {
+    this.init();
+    this.lobbyFetch = setInterval(() => {
+      this.init();
+    }, 2000); //1s
+  }
+
+  init() {
+    this.vgameService.getLobbies().pipe(first()).subscribe(
+      res => {
+        this.LobbyRows = [];
+        res.forEach(lobby => {
+          this.LobbyRows.push({
+            id:                    lobby.id!,
+            name:                  lobby.name!,
+            description:           lobby.description!,
+            currentGame:           lobby.currentGame!,
+            password:              lobby.password!,
+            currentNumberOfPlayer: lobby.currentNumberOfPlayer!,
+            maxPlayer:             lobby.maxPlayer!,
+          })
+        })
+      }
+    );
+  }
+
+  createNewLobby() {
+
   }
 
 }
