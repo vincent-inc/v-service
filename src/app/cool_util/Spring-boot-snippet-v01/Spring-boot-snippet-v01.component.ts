8f54da18cab57f23003ddcb8ea0f36a641a627f6
VincentB00 <vincentbui1999@gmail.com>
2023-06-06 21:28:45 -0400

    add spring boot snippet feature

--- src/app/cool_util/Spring-boot-snippet-v01/Spring-boot-snippet-v01.component.ts
+++ src/app/cool_util/Spring-boot-snippet-v01/Spring-boot-snippet-v01.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Field, SPRINGBOOT_CLASS_V01 } from './spring-boot-v01.model';
 
 @Component({
   selector: 'app-Spring-boot-snippet-v01',
@@ -7,10 +8,269 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SpringBootSnippetV01Component implements OnInit {
 
+  col = 150;
+  row = 60;
+  packageName: string = "";
+  entityName: string = "";
+  detectMethod: string = "auto";
+  fields: Field[] = [];
+  numberOfField: number = 0;
+  inputText: string = "";
+
+  upperCaseEntityName = '';
+  lowerCaseEntityName = '';
+  idFieldDataType = '';
+  idFieldDataNameUpperCase = '';
+  idFieldDataNameLowerCase = '';
+
+  generatedFile: string = "";
+  entityFile: string = "";
+  daoFile: string = '';
+  serviceFile: string = '';
+  controllerFile: string = '';
+
   constructor() { }
 
   ngOnInit() {
-    
+
   }
 
+  updateFieds(): void {
+    while (this.fields.length > this.numberOfField)
+      this.fields.pop();
+
+    while (this.fields.length < this.numberOfField) {
+      let field: Field = { dataType: "String", name: "name" + this.fields.length }
+      this.fields.push(field);
+    }
+  }
+
+  extractField(): void {
+    let newFields: Field[] = [];
+    let splits = this.inputText.split(" ");
+    for (let i = 0; i < splits.length; i++) {
+      let value = splits[i].trim();
+      if (value === "private" || value === "public") {
+        let dataType = splits[i + 1].trim();
+        let name = splits[i + 2].trim().replace(";", "");
+        let field: Field = { dataType: dataType, name: name };
+        newFields.push(field);
+      }
+    }
+
+    this.fields = [...newFields];
+    this.numberOfField = this.fields.length;
+    this.detectMethod = "manual";
+  }
+
+  capitalizeFirstLetter(str: string): string {
+    return str.charAt(0).toUpperCase() + str.slice(1);
+  }
+
+  lowerCaseFirstLetter(str: string): string {
+    return str.charAt(0).toLowerCase() + str.slice(1);
+  }
+
+
+  generatedAll(): void {
+    this.generatedFile = "dao";
+
+    this.upperCaseEntityName = this.capitalizeFirstLetter(this.entityName);
+    this.lowerCaseEntityName = this.lowerCaseFirstLetter(this.entityName);
+    this.idFieldDataType = this.fields[0].dataType!;
+    this.idFieldDataNameUpperCase = this.capitalizeFirstLetter(this.fields[0].name!);
+    this.idFieldDataNameLowerCase = this.lowerCaseFirstLetter(this.fields[0].name!);
+
+    this.generateEntity();
+    this.generateDAO();
+    this.generateService();
+    this.generateController();
+  }
+
+  generateEntity(): void {
+    this.entityFile = SPRINGBOOT_CLASS_V01.ENTITY;
+
+    if (this.fields.length < 1)
+      return;
+
+    this.entityFile = this.replaceStandardVariable(this.entityFile);
+
+    let method = "";
+    if (this.fields.length > 1) {
+      for (let i = 1; i < this.fields.length; i++) {
+        let field = this.fields[i];
+        let lowerCaseName = this.lowerCaseFirstLetter(field.name!);
+
+        method += `\t@Column;\n`
+        method += `\tprivate ${field.dataType} ${lowerCaseName};\n\n`
+      }
+    }
+
+    this.entityFile = this.entityFile.replaceAll("{6}", method);
+  }
+
+  generateDAO(): void {
+    this.daoFile = SPRINGBOOT_CLASS_V01.DAO;
+
+    if (this.fields.length < 1)
+      return;
+
+    this.daoFile = this.replaceStandardVariable(this.daoFile);
+    
+    let method = this.generateDaoMethod();
+
+    this.daoFile = this.daoFile.replaceAll("{7}", method);
+
+    let fieldIdType = this.idFieldDataType;
+
+    switch (fieldIdType) {
+      case "int":
+        fieldIdType = "Integer";
+        break;
+      case "float":
+        fieldIdType = "Float";
+        break;
+      case "double":
+        fieldIdType = "Double";
+        break;
+      case "boolean":
+        fieldIdType = "Boolean";
+        break;
+      default:
+        break;
+    }
+
+    this.daoFile = this.daoFile.replaceAll("{6}", fieldIdType);
+  }
+
+  generateDaoMethod() {
+    let method = '';
+
+    for (let i = 1; i < this.fields.length; i++) {
+      let field = this.fields[i];
+
+      if (!this.isPrimitive(field.dataType!))
+        continue;
+
+      let lowerCaseName = this.lowerCaseFirstLetter(field.name!);
+      let upperCaseName = this.capitalizeFirstLetter(field.name!);
+      let upperCaseEntityName = this.capitalizeFirstLetter(this.entityName);
+
+      method += `\tpublic ${upperCaseEntityName} findBy${upperCaseName}(${field.dataType} ${lowerCaseName});\n`
+      method += `\tpublic List<${upperCaseEntityName}> findAllBy${upperCaseName}(${field.dataType} ${lowerCaseName});\n\n`
+    }
+
+    let upperCaseEntityName = this.capitalizeFirstLetter(this.entityName);
+    let lowerCaseEntityName = this.lowerCaseFirstLetter(this.entityName);
+    let query = `\t@Query(value = "select * from {0} as {1} where {2}", nativeQuery = true)\n`;
+    let method2 = `\tpublic List<{0}> getAllByMatchAll({1});\n\n`;
+    let method3 = `\tpublic List<{0}> getAllByMatchAny({1});\n\n`;
+
+    query = query.replaceAll("{0}", upperCaseEntityName);
+    query = query.replaceAll("{1}", lowerCaseEntityName);
+
+    method2 = method2.replaceAll("{0}", upperCaseEntityName);
+    method3 = method3.replaceAll("{0}", upperCaseEntityName);
+
+    let queryParameter = "";
+    let queryParameter2 = "";
+    let parameter = "";
+
+    for (let i = 1; i < this.fields.length; i++) {
+      let field = this.fields[i];
+      let lowerCaseName = this.lowerCaseFirstLetter(field.name!);
+      let dataType = field.dataType!;
+
+      if (!this.isPrimitive(dataType))
+        continue;
+
+      queryParameter += `${lowerCaseEntityName}.${lowerCaseName} = :${lowerCaseName} and `;
+      queryParameter2 += `${lowerCaseEntityName}.${lowerCaseName} = :${lowerCaseName} or `;
+      parameter += `@Param("${lowerCaseName}") ${dataType} ${lowerCaseName}, `
+    }
+
+    queryParameter = queryParameter.substring(0, queryParameter.length - " and ".length);
+    queryParameter2 = queryParameter2.substring(0, queryParameter2.length - " or ".length);
+    parameter = parameter.substring(0, parameter.length - ", ".length);
+
+    let queryT1 = query.replaceAll("{2}", queryParameter);
+    let queryT2 = query.replaceAll("{2}", queryParameter2);
+    method2 = method2.replaceAll("{1}", parameter);
+    method3 = method3.replaceAll("{1}", parameter);
+
+    method += queryT1 + method2 + queryT2 + method3;
+
+    return method;
+  }
+
+  generateService(): void {
+    this.serviceFile = SPRINGBOOT_CLASS_V01.SERVICE;
+
+    if (this.fields.length < 1)
+      return;
+
+    this.serviceFile = this.replaceStandardVariable(this.serviceFile);
+  }
+
+  generateController(): void {
+    this.controllerFile = SPRINGBOOT_CLASS_V01.CONTROLLER;
+
+    if (this.fields.length < 1)
+      return;
+
+    this.controllerFile = this.replaceStandardVariable(this.controllerFile);
+  }
+
+  /**
+   * {0} package name
+   * {1} uppercase entity name
+   * {2} lowercase entity name
+   * {3} id field data type
+   * {4} id field name lowercase
+   * {5} id field name uppercase
+   */
+  replaceStandardVariable(file: string) {
+    file.replaceAll('{0}', this.packageName);
+    file.replaceAll('{1}', this.upperCaseEntityName);
+    file.replaceAll('{2}', this.lowerCaseEntityName);
+    file.replaceAll('{3}', this.idFieldDataType);
+    file.replaceAll('{4}', this.idFieldDataNameLowerCase);
+    file.replaceAll('{5}', this.idFieldDataNameUpperCase);
+
+    return file;
+  }
+
+  private validFieldDataType(dataType: string): boolean {
+    switch (dataType) {
+      case "float":
+      case "Float":
+      case "double":
+      case "Double":
+      case "int":
+      case "Integer":
+      case "Int":
+        return false;
+      default:
+        return true;
+    }
+  }
+
+  private isPrimitive(dataType: string): boolean {
+    switch (dataType) {
+      case "float":
+      case "Float":
+      case "double":
+      case "Double":
+      case "int":
+      case "Integer":
+      case "Int":
+      case "String":
+      case "char":
+      case "Char":
+      case "boolean":
+        return true;
+      default:
+        return false;
+    }
+  }
 }
---
+++ src/app/cool_util/Spring-boot-snippet-v01/spring-boot-v01.model.ts
@@ -0,0 +1,286 @@
+/**
+ * {0} package name
+ * {1} uppercase entity name
+ * {2} lowercase entity name
+ * {3} id field data type
+ * {4} id field name
+ * {5} id field name uppercase
+ */
+
+export interface Field
+{
+  dataType?: string;
+  name?: string;
+}
+
+const ENTITY = `package {0}.model;
+
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "{2}")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class {1}
+{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private {3};
+
+{6}
+}
+`;
+
+const DAO = `package {0}.dao;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import {0}.model.{1};
+
+public interface {1}Dao extends JpaRepository<{1}, {6}>
+{
+{7}
+}`;
+
+const SERVICE = `package {0}.service;
+
+import java.util.List;
+import org.springframework.stereotype.Service;
+import org.springframework.util.ObjectUtils;
+
+import org.springframework.data.domain.Example;
+import {0}.dao.{1}Dao;
+import {0}.model.{1};
+import {0}.util.DatabaseUtils;
+import {0}.util.ReflectionUtils;
+import {0}.util.Http.HttpResponseThrowers;
+
+@Service
+public class {1}Service {
+    public static final String HASH_KEY = "{0}.service.{1}Service";
+
+    private DatabaseUtils<{1}, Integer> databaseUtils;
+
+    private {1}Dao {2}Dao;
+
+    public {1}Service(DatabaseUtils<{1}, Integer> databaseUtils, {1}Dao {2}Dao) {
+        this.databaseUtils = databaseUtils.init({2}Dao, HASH_KEY);
+        this.{2}Dao = {2}Dao;
+    }
+
+    public List<{1}> getAll() {
+        return this.{2}Dao.findAll();
+    }
+
+    public {1} getBy{5}({3} {4}) {
+        {1} {2} = this.databaseUtils.getAndExpire({4});
+
+        if (ObjectUtils.isEmpty({2}))
+            HttpResponseThrowers.throwBadRequest("{1} ID not found");
+
+        return {2};
+    }
+
+    public {1} tryGetBy{5}({3} {4}) {
+        {1} {2} = this.databaseUtils.getAndExpire({4});
+        return {2};
+    }
+
+    public List<{1}> getAllByMatchAll({1} {2}) {
+        Example<{1}> example = ReflectionUtils.getMatchAllMatcher({2});
+        return this.{2}Dao.findAll(example);
+    }
+
+    public List<{1}> getAllByMatchAny({1} {2}) {
+        Example<{1}> example = ReflectionUtils.getMatchAnyMatcher({2});
+        return this.{2}Dao.findAll(example);
+    }
+
+    public List<{1}> getAllByMatchAll({1} {2}, String matchCase) {
+        Example<{1}> example = ReflectionUtils.getMatchAllMatcher({2}, matchCase);
+        return this.{2}Dao.findAll(example);
+    }
+
+    public List<{1}> getAllByMatchAny({1} {2}, String matchCase) {
+        Example<{1}> example = ReflectionUtils.getMatchAnyMatcher({2}, matchCase);
+        return this.{2}Dao.findAll(example);
+    }
+
+    public {1} create{1}({1} {2}) {
+        this.databaseUtils.saveAndExpire({2});
+        return {2};
+    }
+
+    public {1} modify{1}({3} {4}, {1} {2}) {
+        {1} old{1} = this.getBy{5}({4});
+
+        ReflectionUtils.replaceValue(old{1}, {2});
+
+        old{1} = this.databaseUtils.saveAndExpire(old{1});
+
+        return old{1};
+    }
+
+    public {1} patch{1}({3} {4}, {1} {2}) {
+        {1} old{1} = this.getBy{5}({4});
+
+        ReflectionUtils.patchValue(old{1}, {2});
+
+        old{1} = this.databaseUtils.saveAndExpire(old{1});
+
+        return old{1};
+    }
+
+    public void delete{1}({3} {4}) {
+        this.databaseUtils.deleteBy{5}({4});
+    }
+}`;
+
+const CONTROLLER = `package {0}.controller;
+
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import jakarta.ws.rs.QueryParam;
+import io.swagger.v3.oas.annotations.Operation;
+
+import {0}.model.{1};
+import {0}.service.{1}Service;
+
+@RestController
+@RequestMapping("/{2}s")
+class {1}Controller {
+    @Autowired
+    {1}Service {2}Service;
+
+    @Operation(summary = "Get a list of all {1}")
+    @GetMapping
+    public ResponseEntity<List<{1}>> getAll() {
+        List<{1}> {2}s = {2}Service.getAll();
+
+        if ({2}s.isEmpty())
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+
+        return new ResponseEntity<>({2}s, HttpStatus.OK);
+    }
+
+    @Operation(summary = "Get {1} base on {4} in path variable")
+    @GetMapping("{{4}}")
+    public ResponseEntity<{1}> getBy{5}(@PathVariable("{4}") {3} {4}) {
+        {1} {2} = {2}Service.getBy{5}({4});
+
+        return new ResponseEntity<>({2}, HttpStatus.OK);
+    }
+
+    @Operation(summary = "Get a list of all {1} that match all information base on query parameter")
+    @GetMapping("match_all")
+    public ResponseEntity<List<{1}>> matchAll(@QueryParam("{2}") {1} {2}) {
+        List<{1}> {2}s = this.{2}Service.getAllByMatchAll({2});
+
+        if ({2}s.isEmpty())
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+
+        return new ResponseEntity<>({2}s, HttpStatus.OK);
+    }
+
+    @Operation(summary = "Get a list of all {1} that match any information base on query parameter")
+    @GetMapping("match_any")
+    public ResponseEntity<List<{1}>> matchAny(@QueryParam("{2}") {1} {2}) {
+        List<{1}> {2}s = this.{2}Service.getAllByMatchAny({2});
+
+        if ({2}s.isEmpty())
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+
+        return new ResponseEntity<>({2}s, HttpStatus.OK);
+    }
+
+    @Operation(summary = "Get a list of all {1} that match all information base on query parameter and match case")
+    @GetMapping("match_all/{matchCase}")
+    public ResponseEntity<List<{1}>> matchAll(@QueryParam("{2}") {1} {2},
+            @PathVariable("matchCase") String matchCase) {
+        List<{1}> {2}s = this.{2}Service.getAllByMatchAll({2}, matchCase);
+
+        if ({2}s.isEmpty())
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+
+        return new ResponseEntity<>({2}s, HttpStatus.OK);
+    }
+
+    @Operation(summary = "Get a list of all {1} that match any information base on query parameter and match case")
+    @GetMapping("match_any/{matchCase}")
+    public ResponseEntity<List<{1}>> matchAny(@QueryParam("{2}") {1} {2},
+            @PathVariable("matchCase") String matchCase) {
+        List<{1}> {2}s = this.{2}Service.getAllByMatchAny({2}, matchCase);
+
+        if ({2}s.isEmpty())
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+
+        return new ResponseEntity<>({2}s, HttpStatus.OK);
+    }
+
+    @Operation(summary = "Create a new {1}")
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public ResponseEntity<{1}> create(@RequestBody {1} {2}) {
+        {1} saved{1} = {2}Service.create{1}({2});
+        return new ResponseEntity<>(saved{1}, HttpStatus.CREATED);
+
+    }
+
+    @Operation(summary = "Modify a {1} base on {4} in path variable")
+    @PutMapping("{{4}}")
+    @ResponseStatus(HttpStatus.OK)
+    public ResponseEntity<{1}> update(@PathVariable("{4}") {3} {4}, @RequestBody {1} {2}) {
+        {2} = this.{2}Service.modify{1}({4}, {2});
+        return new ResponseEntity<>({2}, HttpStatus.OK);
+    }
+
+    @Operation(summary = "Patch a {1} base on {4} in path variable")
+    @PatchMapping("{{4}}")
+    @ResponseStatus(HttpStatus.OK)
+    public ResponseEntity<{1}> patch(@PathVariable("{4}") {3} {4}, @RequestBody {1} {2}) {
+        {2} = this.{2}Service.patch{1}({4}, {2});
+        return new ResponseEntity<>({2}, HttpStatus.OK);
+    }
+
+    @Operation(summary = "Delete a {1} base on {4} in path variable")
+    @DeleteMapping("{{4}}")
+    @ResponseStatus(HttpStatus.OK)
+    public ResponseEntity<HttpStatus> delete(@PathVariable("{4}") {3} {4}) {
+        {2}Service.delete{1}({4});
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+}`
+
+export const SPRINGBOOT_CLASS_V01 = {ENTITY: ENTITY, DAO: DAO, SERVICE: SERVICE, CONTROLLER: CONTROLLER}
